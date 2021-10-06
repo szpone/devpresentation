ea34d52665985de96c5d4cc274b416d2a6367d96
Tomek Mańko <jaennirin@gmail.com>
2021-10-06 11:53:33 +0200

    kek

--- devjs/make.sh
+++ devjs/make.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-pandoc -t revealjs -f markdown -s --no-highlight  --slide-level=2 -V revealjs-url=./reveal.js -o index.html presentation.md
+# pandoc -t revealjs -f markdown -s -c assets/styles.css --slide-level=2 -V revealjs-url=./reveal.js -o index.html presentation.md
+pandoc \
+  -t revealjs \
+  --no-highlight \
+  --template=template.html \
+  -f markdown \
+  -s \
+  -c assets/styles.css \
+  -c reveal.js/plugin/highlight/zenburn.css \
+  --slide-level=2 \
+  -V revealjs-url=./reveal.js \
+  -o index.html \
+  presentation.md
